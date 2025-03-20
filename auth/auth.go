83e1c804f370289a4518502ffe1a82bebb2d2aed
root <alewilliam789@gmail.com>
2025-03-20 19:01:22 +0000

    Implement auth w/ JWTs

--- auth/auth.go
+++ auth/auth.go
@@ -1,21 +1,22 @@
 package login
 
 import (
-	// "bytes"
 	"bytes"
 	"context"
 	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
 	"database/sql"
 	"encoding/json"
-  "errors"
-	// "fmt"
-	"crypto/rsa"
-	// "crypto/sha256"
+	"errors"
+	"fmt"
+	"time"
 	"log"
 	"net/http"
+	"strings"
 
 	usersSql "github.com/alewilliam789/go-rest/db"
-	"github.com/redis/go-redis/v9"
+	webToken "github.com/alewilliam789/go-rest/jwt"
 )
 
 const COOKIE_NAME = "user-cookie"
@@ -25,23 +26,29 @@ type LoginAttempt struct {
   Password string `json:"password"`
 }
 
-func getUserMessage(cookieVal string, client *redis.Client) ([]byte, error) {
-  
-  ctx := context.Background()
+func hashPass(loginAttempt *LoginAttempt) error {
+  hasher := sha256.New()
 
-  userMessage, redisErr := client.Get(ctx,cookieVal).Result()
+  loginAttempt.Password = string(hasher.Sum([]byte(loginAttempt.Password)))
 
-  if redisErr != nil {
-    return []byte(""), redisErr
-  }
-  
-  return []byte(userMessage), nil
+  return nil
 }
 
+func checkUserJWT(jwt string, keys *rsa.PrivateKey) error {
 
-func checkUserCookie(msgBytes []byte, keys *rsa.PrivateKey) error {
+  jwtSegments := strings.Split(jwt,".")
 
-  verifyErr := rsa.VerifyPKCS1v15(&keys.PublicKey,crypto.SHA256,msgBytes,nil)
+  if len(jwtSegments) != 3 {
+     return errors.New("Malformed JWT provided")
+  }
+
+  signingString := fmt.Sprintf("%s.%s",jwtSegments[0:0],jwtSegments[1:1])
+  
+  hasher := sha256.New()
+
+  hashed := hasher.Sum([]byte(signingString))
+
+  verifyErr := rsa.VerifyPKCS1v15(&keys.PublicKey,crypto.SHA256,hashed,[]byte(jwtSegments[2]))
   
   if verifyErr != nil {
     return verifyErr
@@ -60,6 +67,8 @@ func checkUserCredentials(loginAttempt *LoginAttempt, queries *usersSql.Queries)
     return userNotFoundErr
   }
 
+  hashPass(loginAttempt)
+
   if !bytes.Equal(foundUser.Password, []byte(loginAttempt.Password)) {
     return errors.New("Mismatching credentials") 
   }
@@ -67,13 +76,38 @@ func checkUserCredentials(loginAttempt *LoginAttempt, queries *usersSql.Queries)
   return nil
 }
 
-func generateCookie() (*http.Cookie, error) {
+func generateCookie(userLogin *LoginAttempt,key *rsa.PrivateKey) (*http.Cookie, error) {
+  
+  var jwt webToken.JWT;
 
-  return nil, nil
+  jwt.New(userLogin.UserName)
+
+  sigErr := jwt.GenerateSig(key)
+
+  if sigErr != nil {
+    return nil, sigErr
+  }
+  
+
+  newCookie := http.Cookie {
+    HttpOnly: true,
+    Name: COOKIE_NAME,
+    Value : jwt.Token,
+  }
+
+  duration, parseErr := time.ParseDuration("48h")
+
+  if parseErr != nil {
+    return nil, parseErr
+  }
+
+  newCookie.Expires.Add(duration)
+
+  return &newCookie, nil
 }
 
 
-func login(w http.ResponseWriter, req *http.Request, queries *usersSql.Queries, client *redis.Client, key *rsa.PrivateKey) {
+func login(w http.ResponseWriter, req *http.Request, queries *usersSql.Queries, key *rsa.PrivateKey) {
     
     // Decode request body into login attempt
     decoder := json.NewDecoder(req.Body)
@@ -96,7 +130,7 @@ func login(w http.ResponseWriter, req *http.Request, queries *usersSql.Queries, 
         log.Print("Incorrect User Credentials")
         return
       }
-      newCookie, cookieErr := generateCookie()
+      newCookie, cookieErr := generateCookie(&loginAttempt,key)
       
       if cookieErr != nil {
         w.WriteHeader(http.StatusInternalServerError)
@@ -109,25 +143,39 @@ func login(w http.ResponseWriter, req *http.Request, queries *usersSql.Queries, 
       return
     }
     
-    _, redisErr := getUserMessage(cookie.Value, client)
-
-    if redisErr != nil {
-      w.WriteHeader(http.StatusInternalServerError)
-      log.Print(redisErr)
-      return
-    }
 
     // Verify that the correct key was used and send on merry way
-  
+    signErr := checkUserJWT(cookie.Value, key)
+
+    if signErr != nil {
+      verificationErr := checkUserCredentials(&loginAttempt, queries)
+
+      if verificationErr != nil {
+        w.WriteHeader(http.StatusUnauthorized)
+        log.Print("Incorrect User Credentials")
+        return
+      }
+
+      newCookie, cookieErr := generateCookie(&loginAttempt, key)
+
+      if cookieErr != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        log.Print("Issue generating cookie")
+        return
+      }
+
+      http.SetCookie(w, newCookie)
+    }
+
     w.WriteHeader(http.StatusOK) 
 }
 
 
-func AuthorizeHandler(w http.ResponseWriter, req *http.Request, db *sql.DB, client *redis.Client, keys *rsa.PrivateKey) {
+func AuthorizeHandler(w http.ResponseWriter, req *http.Request, db *sql.DB, keys *rsa.PrivateKey) {
   
   queries := usersSql.New(db)
   
   if req.Method == "POST" {
-    login(w,req,queries,client,keys)
+    login(w,req,queries,keys)
   }
 }
